00919f48902942ce54c4edbbcaee6024f8b6f009
Jayendra Pawar <jayendra.pawar@ibm.com>
2026-01-10 21:39:26 +0530

    thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,22 +16,32 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://jayendrapawar-resume.vercel.app"),
 
   title: "Jayendra Pawar — Backend Engineer",
-  description: "Golang • Kubernetes • Distributed Systems • AWS",
+  description:
+    "Specialized Backend Engineer in Golang, Kubernetes, Distributed Systems, and AWS. Experienced in building scalable, reliable backend platforms.",
 
   openGraph: {
     title: "Jayendra Pawar — Backend Engineer",
-    description: "Golang • Kubernetes • Distributed Systems • AWS",
+    description:
+      "Specialized Backend Engineer in Golang, Kubernetes, Distributed Systems, and AWS. Experienced in building scalable, reliable backend platforms.",
+    url: "https://jayendrapawar-resume.vercel.app/",
     siteName: "Jayendra Pawar",
-    url: "https://jayendrapawar-resume.vercel.app",
     images: [
       {
         url: "https://jayendrapawar-resume.vercel.app/JayendraPawarCC.png",
         width: 1200,
         height: 630,
-        alt: "Jayendra Pawar Resume & Portfolio",
+        alt: "Jayendra Pawar — Backend Engineer",
       },
     ],
     type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Jayendra Pawar — Backend Engineer",
+    description:
+          "Specialized Backend Engineer in Golang, Kubernetes, Distributed Systems, and AWS. Experienced in building scalable, reliable backend platforms.",
+    images: ["https://jayendrapawar-resume.vercel.app/JayendraPawarCC.png"],
   },
 };
 
